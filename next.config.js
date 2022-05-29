677f2d9d564dc17f47565625084c1d800748f849
ruslanNir <nirkuba19999@gmail.com>
2022-05-29 14:54:31 +0300

    config for images and upfdate json packege

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+   reactStrictMode: true,
+   images: {
+      domains: ["avatars.githubusercontent.com"],
+      // formats: ["image/webp"],
+   },
 }
 
 module.exports = nextConfig
