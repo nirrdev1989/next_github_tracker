fb63af24772c609b645565f678917769935f54f1
ruslanNir <nirkuba19999@gmail.com>
2022-06-21 16:36:54 +0300

    read more coponent

---
+++ components/ReadMore/ReadMore.tsx
@@ -0,0 +1,34 @@
+import { HTMLAttributes, DetailedHTMLProps, ReactNode, useState } from "react"
+import styles from "./ReadMore.module.css"
+
+
+interface ReadMoreProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+   // children: ReactNode
+   text: string
+   lengthLimit: number
+}
+
+export default function ReadMore({ text, lengthLimit }: ReadMoreProps) {
+   const [open, setOpen] = useState<boolean>(false)
+
+
+   function toggleText() {
+      setOpen((prev) => !prev)
+   }
+
+   let limitText = text
+
+   if (!open) {
+      limitText = limitText?.slice(0, lengthLimit)
+   }
+
+   if (!text) {
+      return null
+   }
+   return (
+      <>
+         <span className={`${styles.read_more_text} ${open ? styles.open : ""}`}> {limitText}</span>
+         {text.length > lengthLimit && <div className={styles.read_more_btn} onClick={toggleText} >{open ? "Read less" : "Read more"}</div>}
+      </>
+   )
+}
